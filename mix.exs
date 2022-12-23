69af27cdb1ed4593de3b77a3a2a5309d6536c291
Matheus Alvarenga França <matheus.alvfr@gmail.com>
2022-12-23 13:06:34 -0300

    Add mnemonic slugs dependency

--- mix.exs
+++ mix.exs
@@ -44,7 +44,8 @@ defmodule PersonalityTest.MixProject do
       {:telemetry_metrics, "~> 0.6"},
       {:telemetry_poller, "~> 1.0"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:mnemonic_slugs, "~> 0.0.3"}
     ]
   end
 
