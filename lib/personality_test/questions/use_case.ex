93d52fae9229383e05810c3c2189805852ee6b7a
Matheus Alvarenga França <matheus.alvfr@gmail.com>
2022-12-24 15:34:20 -0300

    Implement the questionnaire - Reading data from in-memory database and listing questions one by one

--- lib/personality_test/questions/use_case.ex
+++ lib/personality_test/questions/use_case.ex
@@ -1,7 +1,26 @@
 defmodule PersonalityTest.Questions.UseCase do
+  alias PersonalityTest.Questions.InMemoryDb
   alias PersonalityTest.Util.Json
 
   def load_questions(file_path) do
     Json.read_json_file(file_path)
+  end
+
+  def initialize_db(slug, questions) do
+    InMemoryDb.put(slug, %{questions: questions, current_question: 0, total_score: 0})
+  end
+
+  def increment_current_question(slug) do
+    slug
+    |> InMemoryDb.get()
+    |> update_in([:current_question], &(&1 + 1))
+    |> then(fn state -> InMemoryDb.put(slug, state) end)
+  end
+
+  def increment_score(slug, score) do
+    slug
+    |> InMemoryDb.get()
+    |> update_in([:total_score], &(&1 + score))
+    |> then(fn state -> InMemoryDb.put(slug, state) end)
   end
 end
--- lib/personality_test_web/live/personality_test_live.ex
+++ lib/personality_test_web/live/personality_test_live.ex
@@ -1,13 +1,73 @@
 defmodule PersonalityTestWeb.PersonalityTestLive do
+  alias PersonalityTest.Questions.InMemoryDb
+  alias PersonalityTest.Questions
+
   use PersonalityTestWeb, :live_view
 
-  def mount(_params, _session, socket) do
+  @impl true
+  def mount(params, _session, socket) do
+    questions = Questions.UseCase.load_questions("resources/questions.json")
+    slug = params["id"]
+
+    socket =
+      slug
+      |> InMemoryDb.get()
+      |> case do
+        nil ->
+          Questions.UseCase.initialize_db(slug, questions)
+
+          socket
+          |> assign(:questions, questions)
+          |> assign(:current_question, 0)
+
+        data ->
+          socket
+          |> assign(:questions, data.questions)
+          |> assign(:current_question, data.current_question)
+      end
+      |> assign(:slug, slug)
+
     {:ok, socket}
   end
 
+  @impl true
+  def handle_event("save", params, socket) do
+    current_question = socket.assigns.current_question
+    total_questions = length(socket.assigns.questions)
+    slug = socket.assigns.slug
+
+    if current_question < total_questions - 1 do
+      socket = socket |> assign(:current_question, current_question + 1)
+
+      Questions.UseCase.increment_current_question(slug)
+      Questions.UseCase.increment_score(slug, String.to_integer(params["question_weight"]))
+
+      {:noreply, socket}
+    else
+      {:noreply, push_redirect(socket, to: "/result?id=#{socket.assigns.slug}")}
+    end
+  end
+
+  @impl true
   def render(assigns) do
+    row = Enum.at(assigns.questions, assigns.current_question)
+
     ~H"""
-     <main class="col-md-6 mx-auto px-3 rounded-3 border shadow-lg p-3 mb-5 bg-body">
+     <main class="col-md-12 mx-auto px-3 rounded-3 border shadow-lg p-3 mb-5 bg-body">
+      <div class="d-flex justify-content-between p-1">
+        <h3 style= "color: #22A39F" class="my-1"> <%= row.dimension %> </h3>
+        <h3 style= "color: #22A39F" class="my-1"> <%= assigns.current_question + 1%> / <%= length(assigns.questions) %> </h3>
+      </div>
+       <hr/>
+       <h4> <%= row.question%> </h4>
+         <.form let={_f} for={:questions} phx-submit="save">
+             <%= for answer <- row.answers  do %>
+               <div class="form-check p-3 ms-2">
+                 <input required type="radio" class="form-check-input" name="question_weight" value={answer.weight}><%= answer.text %>
+               </div>
+             <% end %>
+             <%= submit("Next", class: "btn btn-md btn-secondary fw-bold border-white") %>
+         </.form>
     </main>
     """
   end
