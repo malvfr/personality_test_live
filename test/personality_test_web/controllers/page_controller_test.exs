c6cf37b8dcb76a9a6d6d61968e255ad4497ad0d3
Matheus Alvarenga França <matheus.alvfr@gmail.com>
2022-12-24 23:49:08 -0300

    Add page controller tests

--- test/personality_test_web/controllers/page_controller_test.exs
+++ test/personality_test_web/controllers/page_controller_test.exs
@@ -1,8 +1,27 @@
 defmodule PersonalityTestWeb.PageControllerTest do
+  alias PersonalityTest.Questions.InMemoryDb
   use PersonalityTestWeb.ConnCase
 
-  test "GET /", %{conn: conn} do
-    conn = get(conn, "/")
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+  test "GET /result without a valid InMemoryDB state, redirects user to the landing page", %{
+    conn: conn
+  } do
+    conn = get(conn, "/result", %{"id" => "test"})
+    assert redirected_to(conn, 302) == "/"
+  end
+
+  test "GET /result with a valid InMemoryDB state, renders the correct page", %{
+    conn: conn
+  } do
+    InMemoryDb.put("test_slug", %{total_score: 100})
+
+    conn = get(conn, "/result", %{"id" => "test_slug"})
+    assert html_response(conn, 200)
+  end
+
+  test "POST / should create a new slug and redirect to it", %{
+    conn: conn
+  } do
+    conn = post(conn, "/")
+    assert redirected_to(conn, 302) =~ "/personality/"
   end
 end
